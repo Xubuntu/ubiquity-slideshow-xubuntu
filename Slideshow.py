d26eae9f0893f8ffe0f993c3d0d73250f2fcbe64
Dylan McCall <dmccall@dylan-laptop>
2009-06-19 09:24:04 -0700

    Slideshow.py:
    Added rgba support for the sake of it
    Added progress bar, which increments by 1% every 2 seconds.

--- Slideshow.py
+++ Slideshow.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python
 '''
 Simple program to test the slideshow using a real webkit.WebView widget.
+
+Please excuse its horrifying ugliness.
 '''
 
 import os, sys
 import gtk
+import gobject
 import webkit
 
 base_directory = os.path.dirname (sys.argv[0])
 
-gtk.threads_init()
+gtk.gdk.threads_init()
 
 slideshow_window = gtk.Window()
 slideshow_window.set_title("Ubiquity Slideshow with Webkit")
@@ -30,6 +33,35 @@ slideshow_webview.set_size_request(700,420)
 
 slideshow_container.add(slideshow_webview)
 
+install_progressbar = gtk.ProgressBar()
+install_progressbar.set_text("Pretending to install. Please wait...")
+install_progressbar.set_fraction(0)
+
+slideshow_container.add(install_progressbar)
+
+#RGBA! Hooray!
+colormap = slideshow_window.get_screen().get_rgba_colormap()
+if colormap:
+	print('We are RGBA`d!')
+	gtk.widget_set_default_colormap(colormap)
+
 slideshow_window.show_all()
 
+
+def progress_increment(progressbar, fraction):
+	new_fraction = progressbar.get_fraction() + fraction
+	if new_fraction > 1:
+		progressbar.set_fraction(1.0)
+		install_progressbar.set_text("Finished pretending to install.")
+		return False
+	
+	progressbar.set_fraction(new_fraction)
+	install_progressbar.set_text("Pretending to install... %d%%" % (new_fraction * 100))
+	return True
+
+install_timer = gobject.timeout_add_seconds(2, progress_increment, install_progressbar, 0.01)
+
 gtk.main()
+
+gtk.widget_pop_colormap()
+print('Cleaned up GTK colormap stuff')
