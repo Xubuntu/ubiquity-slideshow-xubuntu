59322b02f1781c0a0727d3d7c0ea79a68d34ec39
Dylan McCall <dmccall@dylan-laptop>
2009-07-24 15:40:05 -0700

    Slideshow.py:
    * Minor tidying. The structure is now slightly easier to follow (though still rather unhelpful).
    * Ideal dimensions for slideshow are stored in slideshow.conf then read by Slideshow.py using ConfigParser.
    * Moved progress bar to top of window and added a height request to make it taller. (30px).
    * Added padding between progress bar and slideshow.
    * Added window contents to an Alignment widget, with extra padding.
    * enable-developer-extras removed enitrely; it didn't serve a major purpose, and we don't support multiple windows anyway.

--- Slideshow.py
+++ Slideshow.py
@@ -8,46 +8,60 @@ Please excuse its horrifying ugliness.
 import os, sys
 import gtk
 import gobject
+import ConfigParser
 import webkit
 
 base_directory = os.path.dirname (sys.argv[0])
+slideshow_path = os.path.abspath(base_directory)
+
+slideshow_config = ConfigParser.ConfigParser()
+slideshow_config.read(os.path.join(slideshow_path,'slideshow.conf'))
+
 
 gtk.gdk.threads_init()
 
 slideshow_window = gtk.Window()
 slideshow_window.set_title("Ubiquity Slideshow with Webkit")
 slideshow_window.connect('destroy',gtk.main_quit)
-
-slideshow_container = gtk.VBox()
-slideshow_window.add(slideshow_container)
-
-
-slideshow_webview = webkit.WebView()
-settings = slideshow_webview.get_settings()
-try:
-    settings.set_property("enable-developer-extras", True)
-except TypeError:
-    # Webkit in 9.04 does not support enable-developer-extras
-    pass
-#TODO: Disable right click menus and resond to the user pressing links
-
-#TODO: Parse a configuration file to get the following information
-slideshow_webview.open( os.path.abspath(os.path.join(base_directory, "slides", "index.html")))
-slideshow_webview.set_size_request(700,420)
-
-slideshow_container.add(slideshow_webview)
-
-install_progressbar = gtk.ProgressBar()
-install_progressbar.set_text("Pretending to install. Please wait...")
-install_progressbar.set_fraction(0)
-
-slideshow_container.add(install_progressbar)
-
-#RGBA! Hooray!
+#Setting up rgba colourmap because we can :)
 colormap = slideshow_window.get_screen().get_rgba_colormap()
 if colormap:
 	print('We are RGBA`d!')
 	gtk.widget_set_default_colormap(colormap)
+
+slideshow_window_align = gtk.Alignment()
+slideshow_window_align.set_padding(8,8,8,8)
+#Note there's probably a convention for padding that I'm forgetting here
+slideshow_window.add(slideshow_window_align)
+
+slideshow_container = gtk.VBox()
+slideshow_container.set_spacing(8)
+slideshow_window_align.add(slideshow_container)
+
+
+slideshow_webview = webkit.WebView()
+
+config_width = int(slideshow_config.get('Slideshow','width'))
+config_height = int(slideshow_config.get('Slideshow','height'))
+slideshow_webview.set_size_request(config_width,config_height)
+
+# settings = slideshow_webview.get_settings()
+#uncomment if we need to change settings
+
+#TODO: Disable right click menus and resond to the user pressing links
+
+slideshow_webview.open( os.path.join(slideshow_path, "slides", "index.html"))
+
+
+install_progressbar = gtk.ProgressBar()
+install_progressbar.set_size_request(-1,30)
+install_progressbar.set_text("Pretending to install. Please wait...")
+install_progressbar.set_fraction(0)
+
+
+slideshow_container.add(install_progressbar)
+slideshow_container.add(slideshow_webview)
+
 
 slideshow_window.show_all()
 
