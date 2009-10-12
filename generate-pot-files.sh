6af22a616f94bbcdfbee5a8d8c4c101fdf311dd4
Dylan McCall <dylan@dylan-laptop>
2009-10-12 16:50:41 -0700

    Modified generate-pot-files to accept specific slides passed as arguments. In that case, it only generates pot files for those ones, instead of for all of the slides.
    Minor change to installing-software.html and its translation template.
    Switched "making" and "creating" and changed "galaxy" to "universe".
    This lines up with keywords one can search for and get awesome results from Software Center.

--- generate-pot-files.sh
+++ generate-pot-files.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 set -e
-for slide in slides/*.html; do
+if [ $# -gt 0 ]; then
+	slides="$@"
+	echo "argument handled?"
+else
+	slides="slides/*.html"
+fi
+for slide in $slides; do
 	slidename="$(basename $slide)"
 	[ "$slidename" = "index.html" ] && continue
 	mkdir -p "po/$slidename"
 	html2po -P $slide -o "po/$slidename/template.pot"
 done
+
