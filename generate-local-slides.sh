6de8afa8738ad37a8a65f0226230c56d76422459
Evan Dandrea <evan.dandrea@canonical.com>
2010-02-12 11:22:39 +0000

    Tidy up generate-local-slides.sh and make it less noisy.

--- generate-local-slides.sh
+++ generate-local-slides.sh
@@ -12,49 +12,49 @@ BUILD=build
 PODIR=$SOURCE/po/$distro
 BUILDSLIDES=$BUILD/$distro/slides
 
-echo "directory = new Object();" >> $BUILDSLIDES/directory.js;
+echo "directory = new Object()" >> $BUILDSLIDES/directory.js
 
 for slide in $BUILDSLIDES/*.html; do
-	newslide="$slide.new";
-	mv $slide $newslide;
+	newslide="$slide.new"
+	mv $slide $newslide
 	#htmlclean removes all comments and whitespace, leaves behind *.bak
-	#htmlclean $newslide && rm "$newslide.bak";
+	#htmlclean $newslide && rm "$newslide.bak"
 	#run po2html on source slides for consistent formatting
-	po2html --notidy --progress=none -i po/null.po -t $newslide -o $slide;
-	rm $newslide;
-done;
+	po2html --notidy --progress=none -i po/null.po -t $newslide -o $slide
+	rm $newslide
+done
 
-if which po2html; then
-	for locale in $PODIR/*.po; do
-		if [ -e $locale ]; then
-			localename=$(basename $locale .po);
-			localeslides=$BUILDSLIDES/loc.$localename;
+if ! which po2html; then
+	echo; echo "Error: po2html is not available."
+	exit 1
+fi
+for locale in $PODIR/*.po; do
+	if [ -e $locale ]; then
+		localename=$(basename $locale .po)
+		localeslides=$BUILDSLIDES/loc.$localename
+		
+		echo "directory['$localename'] = new Object()" >> $BUILDSLIDES/directory.js
+		echo "Found locale: $locale"
+		
+		for slide in $BUILDSLIDES/*.html; do
+			slidename=$(basename $slide)
+			[ $slidename = "index.html" ] && continue
 			
-			echo "directory['$localename'] = new Object();" >> $BUILDSLIDES/directory.js;
-			echo "Found locale: $locale";
+			outputslide="$localeslides/$slidename"
+			[ -e $outputslide ] && rm -f $outputslide
+			[ ! -e $localeslides ] && mkdir -p $localeslides
 			
-			for slide in $BUILDSLIDES/*.html; do
-				slidename=$(basename $slide);
-				[ $slidename = "index.html" ] && continue;
-				
-				outputslide="$localeslides/$slidename";
-				[ -e $outputslide ] && rm -f $outputslide;
-				[ ! -e $localeslides ] && mkdir -p $localeslides;
-				
-				po2html --notidy --progress=names -i $locale -t $slide -o $outputslide;
-				cmp $slide $outputslide;
-				if [ $? -eq 0 ]; then
-					#remove new slide if it's the same as the default
-					rm $outputslide;
-					rmdir $localeslides; #will only succeed if dir is empty
-					echo "$slide was not translated for $locale";
-				else
-					echo "directory['$localename']['$slidename'] = true;" >> $BUILDSLIDES/directory.js;
-					echo "$slide was translated for $locale locale";
-				fi;
-			done;
-		fi;
-	done;
-else
-	echo "\nError: po2html is not available.";
-fi;
+			po2html --notidy --progress=names -i $locale -t $slide -o $outputslide 2>/dev/null
+			cmp -s $slide $outputslide
+			if [ $? -eq 0 ]; then
+				#remove new slide if it's the same as the default
+				rm $outputslide
+				rmdir $localeslides 2>/dev/null || true
+				echo "$slide was not translated for $locale"
+			else
+				echo "directory['$localename']['$slidename'] = true" >> $BUILDSLIDES/directory.js
+				echo "$slide was translated for $locale locale"
+			fi
+		done
+	fi
+done
