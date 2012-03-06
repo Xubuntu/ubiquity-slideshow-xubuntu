5adc8bbb8599b248e5e9ef5dad505a79e0965e7e
Michael Rawson <michaelrawson76@gmail.com>
2012-03-06 23:35:51 +0000

    Fixed bug #948549

--- test-slideshow.sh
+++ test-slideshow.sh
@@ -21,5 +21,5 @@ slideshow=$(zenity --list --radiolist --column="Pick" --column="Slideshow" $slid
 
 mv "$BUILD" "$BUILD.backup" 2>/dev/null
 trap "[ -e "$BUILD.backup" ] && rm -rf "$BUILD" ; mv "$BUILD.backup" "$BUILD"" 0 1 2 15
-make build_$slideshow | tee >(zenity --progress --pulsate --title="$TITLE" --text="Building temporary slideshow for testing.\n<i>(make build_$slideshow)</i>" --auto-close)
+make build_$slideshow | tee | zenity --progress --pulsate --title="$TITLE" --text="Building temporary slideshow for testing.\n<i>(make build_$slideshow)</i>" --auto-close
 ./Slideshow.py --path="$BUILD/$slideshow" --controls
