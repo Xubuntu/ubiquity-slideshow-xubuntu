88682778b25309be8f2d2d61501c3d2bac1c8840
Pasi Lallinaho <pasi@shimmerproject.org>
2012-08-27 19:24:58 +0300

    Editing the test-slideshow.sh to accept a slideshow as a parameter. Now, if you run it with eg. "./test-slideshow.sh xubuntu", it will skip the Zenity UI and run the "xubuntu" slideshow. Running "./test-slideshow.sh" still opens the slideshow picker dialog.

--- test-slideshow.sh
+++ test-slideshow.sh
@@ -6,18 +6,22 @@ SOURCE=.
 BUILD=$SOURCE/build
 SOURCESLIDES=$SOURCE/slideshows
 
-slideshows=""
-for show in $SOURCESLIDES/*; do
-	showname=$(basename $show)
-	#oddly placed files we need to ignore
-	[ $showname = "link-core" ] && continue
-	#if we're still going, add this slideshow to the list
-	select=FALSE
-	[ $showname = "ubuntu" ] && select=TRUE
-	slideshows="$slideshows $select $showname"
-done
-slideshow=$(zenity --list --radiolist --column="Pick" --column="Slideshow" $slideshows --title="$TITLE" --text="Choose a slideshow to test")
-[ "$slideshow" = "" ] | [ "$slideshow" = "(null)" ] && exit
+slideshow=$1
+if [ -z "$slideshow" ]
+	then
+		slideshows=""
+		for show in $SOURCESLIDES/*; do
+			showname=$(basename $show)
+			#oddly placed files we need to ignore
+			[ $showname = "link-core" ] && continue
+			#if we're still going, add this slideshow to the list
+			select=FALSE
+			[ $showname = "ubuntu" ] && select=TRUE
+			slideshows="$slideshows $select $showname"
+		done
+		slideshow=$(zenity --list --radiolist --column="Pick" --column="Slideshow" $slideshows --title="$TITLE" --text="Choose a slideshow to test")
+		[ "$slideshow" = "" ] | [ "$slideshow" = "(null)" ] && exit
+fi
 
 mv "$BUILD" "$BUILD.backup" 2>/dev/null
 trap "[ -e "$BUILD.backup" ] && rm -rf "$BUILD" ; mv "$BUILD.backup" "$BUILD"" 0 1 2 15
